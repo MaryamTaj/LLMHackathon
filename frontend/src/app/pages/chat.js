62c682c7590d8fc45d5d221e11918a62eb678a60
Pratibha <108570654+praticodes@users.noreply.github.com>
2024-10-04 19:49:24 -0400

    landing page

---
+++ frontend/src/app/pages/chat.js
@@ -0,0 +1,14 @@
+"use client"; 
+
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to the Chat!</h1>
+      {/* Add chat functionality here */}
+    </div>
+  );
+};
+
+export default Chat;
